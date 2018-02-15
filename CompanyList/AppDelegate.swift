d4b1e313743eaa7f5898f18a203eebe5c8b51b3c
Jonathan Kim <jonnokim17@gmail.com>
2018-02-15 15:53:30 -0800

    basic setup for root viewcontroller

--- CompanyList/AppDelegate.swift
+++ CompanyList/AppDelegate.swift
@@ -13,9 +13,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+
+        window = UIWindow()
+        window?.makeKeyAndVisible()
+
+        let companiesController = ViewController()
+        let navController = CustomNavigationController(rootViewController: companiesController)
+        window?.rootViewController = navController
+
         return true
     }
 
---
+++ CompanyList/CustomNavigationController.swift
@@ -0,0 +1,15 @@
+//
+//  CustomNavigationController.swift
+//  CompanyList
+//
+//  Created by Jonathan Kim on 2/15/18.
+//  Copyright © 2018 Jonathan Kim. All rights reserved.
+//
+
+import UIKit
+
+class CustomNavigationController: UINavigationController {
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+}
